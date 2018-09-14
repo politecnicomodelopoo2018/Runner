27b2b8ad663555823fea71df3c95024ef3439209
tobiasrf22 <tobiasrf22@gmail.com>
2018-09-14 11:20:34 -0300

    https://github.com/politecnicomodelopoo2017/ProyectoMario

--- Borrar.py
+++ Borrar.py
@@ -1,16 +1,17 @@
 import pygame
 from Clases.player.player import Player
-p = Player(400,400)
+p = Player(300,300)
 screen = pygame.display.set_mode((600, 600))
 
 all_sprites = pygame.sprite.Group()
 black = (0,0,0)
 all_sprites.add(p)
+screen.fill(black)
 while True:
 
-    screen.fill(black)
+
 
     all_sprites.draw(screen)
-
-
-
+    pygame.display.update()
+    pygame.display.flip()
+    pygame.time.wait(1)
--- Clases/player/player.py
+++ Clases/player/player.py
@@ -5,9 +5,8 @@ class Player(pygame.sprite.Sprite):
 
 
     def __init__(self,x,y):
-
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('imagenes/Player/Player.png')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
